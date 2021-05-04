1ce033f3298b148aa288676214de1deed7cf68a6
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-05-05 01:06:20 +0530

    Frontend First Step

--- client/src/Component/Home/HomeComponents.js
+++ client/src/Component/Home/HomeComponents.js
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+export const Section = styled.div`
+    display: grid;
+    place-items: center;
+    background-color: #202950;
+    width: 100%;
+    height: 100vh;
+`
--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
-import Home from './Component';
+import Home from './Component/Home';
 
 function Main(){
 
